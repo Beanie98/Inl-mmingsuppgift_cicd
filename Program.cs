15a0ea9acec6d9d5ab83b2980b704cf27ed01420
Beanie98 <mirandapalm74@gmail.com>
2024-02-08 00:59:09 +0100

    Implemented encryption and decryption

--- Program.cs
+++ Program.cs
@@ -1,20 +1,58 @@
+using Bean;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+var crypter = new EncryptDecrypt();
+
 app.MapGet("/", () => "Hello Hello!!!!");
 
-app.MapGet("/encrypt", (string user_input) => Encrypt(user_input) );
-app.MapGet("/decrypt", (string user_input) => Decrypt(user_input) );
+app.MapGet("/encrypt", (string user_input) => crypter.Encrypt(user_input));
+app.MapGet("/decrypt", (string user_input) => crypter.Decrypt(user_input));
 
 app.Run();
 
-string Encrypt(string input)
+namespace Bean
 {
-    return "Hello!! :D";
-}
+    public class EncryptDecrypt
+    {
+        readonly char[] Alphabet = new char[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+        public string Encrypt(string input)
+        {
+            input = input.ToLower();
 
-string Decrypt(string input)
-{
-    return "Goodbye!! D:";
-}
+            char[] secretMessage = input.ToCharArray();
+            char[] encryptedMessage = new char[secretMessage.Length];
 
+            for (int i = 0; i < secretMessage.Length; i++)
+            {
+                char letter = secretMessage[i];
+                int letterPos = Array.IndexOf(Alphabet, letter);
+                int newLetterPos = (letterPos + 3) % Alphabet.Length;
+                char letterEncoded = Alphabet[newLetterPos];
+                encryptedMessage[i] = letterEncoded;
+            }
+            string encodedString = string.Join("", encryptedMessage);
+            return encodedString;
+        }
+
+        public string Decrypt(string input)
+        {
+            char[] encryptedMessage = input.ToCharArray();
+            char[] decryptedMessage = new char[encryptedMessage.Length];
+
+            for (int i = 0; i < encryptedMessage.Length; i++)
+            {
+                char letter = encryptedMessage[i];
+                int letterPos = Array.IndexOf(Alphabet, letter);
+                int newLetterPos = (letterPos - 3 + Alphabet.Length) % Alphabet.Length;
+                char letterDecoded = Alphabet[newLetterPos];
+                decryptedMessage[i] = letterDecoded;
+            }
+
+            string decodedString = new string(decryptedMessage);
+            return decodedString;
+        }
+
+    }
+}
