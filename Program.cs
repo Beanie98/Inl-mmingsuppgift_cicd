6d3f2339d37eaa30d28a9aefd69d5d9e182d2881
Beanie98 <mirandapalm74@gmail.com>
2024-02-08 00:48:05 +0100

    added dummy encryption/dectryption

--- Program.cs
+++ Program.cs
@@ -3,4 +3,18 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello Hello!!!!");
 
+app.MapGet("/encrypt", (string user_input) => Encrypt(user_input) );
+app.MapGet("/decrypt", (string user_input) => Decrypt(user_input) );
+
 app.Run();
+
+string Encrypt(string input)
+{
+    return "Hello!! :D";
+}
+
+string Decrypt(string input)
+{
+    return "Goodbye!! D:";
+}
+
